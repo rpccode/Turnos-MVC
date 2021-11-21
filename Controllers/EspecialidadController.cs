5dde7b822c30ecb65a21c06afa9cf1adc3dfc5b0
Rudy Casilla <rpccode@gmail.com>
2021-11-21 08:37:46 -0500

    Iniciando Con la Conexion a la base de datos

---
+++ Controllers/EspecialidadController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Turnos.Controllers
+{
+    public class EspecialidadController : Controller
+    {
+        public EspecialidadController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
