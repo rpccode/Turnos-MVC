febd1fec3fba8986837780d6afcf610b68c76279
Rudy Casilla <rpccode@gmail.com>
2021-11-21 10:15:47 -0500

    Configurando Entity

---
+++ Models/TurnosContex.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Turnos.Models
+{
+    public class TurnosContext : DbContext
+    {
+        public TurnosContext(DbContextOptions<TurnosContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Especialidad> Especialidad { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Turnos.Models;
 
 namespace Turnos
 {
@@ -24,6 +26,10 @@ namespace Turnos
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<TurnosContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("TurnosContex"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
