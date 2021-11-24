65868ceb29fe65c3381a2e3ee45c6b44b2835ae5
Rudy Casilla <rpccode@gmail.com>
2021-11-23 20:15:54 -0500

    Migrando Modelo a la  de datos

--- Migrations/20211124011343_MigracionPaciente.Designer.cs
+++ Migrations/20211124011343_MigracionPaciente.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Turnos.Models;
 
 namespace Turnos.Migrations
 {
     [DbContext(typeof(TurnosContext))]
-    partial class TurnosContextModelSnapshot : ModelSnapshot
+    [Migration("20211124011343_MigracionPaciente")]
+    partial class MigracionPaciente
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
