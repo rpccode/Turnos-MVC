0c4702bd7c7117749322a702438aa8cbfc198a3c
Rudy Casilla <rpccode@gmail.com>
2021-11-25 18:28:10 -0500

    Metodo Detalles paciente Creado

--- Controllers/PacienteController.cs
+++ Controllers/PacienteController.cs
@@ -18,6 +18,24 @@ namespace Turnos.Controllers
             return View(await _context.Paciente.ToListAsync());
         }
 
+        public async Task<IActionResult> Details(int? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var paciente = await _context.Paciente.FirstOrDefaultAsync(p => p.IdPaciente == id);
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+
+            return View(paciente);
+
+        }
+
 
 
     }
